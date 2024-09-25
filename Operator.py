82fcd1d308c236850b97f1f7eadec7fd2e026793
dkainthan <dkainthan@gmail.com>
2024-09-25 10:08:24 -0400

    Update Operator.py

---
+++ Operator.py
@@ -0,0 +1,23 @@
+#Diya Kainthan
+#9/24/2024
+#Operator Practice
+print(5*(2-3))
+print("2 + 2 =", end = ' ')
+print(2+2)
+print("2 + 2 is", 2+2, "as you can see")
+print(9/5) 
+print(9.0/5) 
+print(9/5.0) 
+print(9.0/5.0) 
+print(9//5) 
+print(9.0//5) 
+print(9//5.0) 
+print(9.0//5.0) 
+print(27//5)
+print(27%5)
+print(9//4)
+print(9%4)
+print(21//2)
+print(21%2)
+print(5//8)
+print(5%8)
